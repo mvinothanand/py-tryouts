43f079c6a162ea83196727d2e8151d11ccbb9636
vinoth masilamani <vinoth.anand@gmail.com>
2024-03-06 19:22:26 +0530

    gcp cloud storage example

---
+++ gcp/cloud_storage_single_upload.py
@@ -0,0 +1,40 @@
+from google.cloud import storage
+
+
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    # The ID of your GCS bucket
+    bucket_name = bucket_name
+    # The path to your file to upload
+    source_file_name = source_file_name
+    # The ID of your GCS object
+    # destination_blob_name = "storage-object-name"
+    destination_blob_name = destination_blob_name
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    # Optional: set a generation-match precondition to avoid potential race conditions
+    # and data corruptions. The request to upload is aborted if the object's
+    # generation number does not match your precondition. For a destination
+    # object that does not yet exist, set the if_generation_match precondition to 0.
+    # If the destination object already exists in your bucket, set instead a
+    # generation-match precondition using its generation number.
+    generation_match_precondition = 0
+
+    blob.upload_from_filename(source_file_name, if_generation_match=generation_match_precondition)
+
+    print(
+        f"File {source_file_name} uploaded to {destination_blob_name}."
+    )
+
+def main():
+  bucket_name = "tryouts_tci"
+  source_file_name = "/home/vinoth/Documents/tci/tciTimeline/20240305/guardian-055645-01.jpg"
+  destination_blob_name = "/guardian/2024/poster/guardian-055645-01.jpg"
+  upload_blob(bucket_name, source_file_name, destination_blob_name)
+
+
+if __name__ == "__main__":
+   main()
